cdf764f82ed89a87ad306f57d138f50d610bfc19
Brent <brentw.white@gmail.com>
2015-11-16 13:28:54 -1000

    homepage layout finished

--- src/js/slidenav.js
+++ src/js/slidenav.js
@@ -1,3 +1,18 @@
+$(window).scroll(function(){
+    // select slide nav
+    var slideNavtrigger = $('.slide-nav-trigger');
+    // if user scrolls past 200px show slide-nav-trigger
+    if($(window).scrollTop() > 500){
+
+        slideNavtrigger.css('visibility','visible');
+        $('.slide-nav-trigger').addClass('show');
+    }else if($(window).scrollTop() < 600){
+        slideNavtrigger.css('visibility','hidden');
+    }
+});
+
+
+
 $(function(){
 // assign click event to slide-nav-trigger
 var slideNav = $('.slide-nav');
