98c3dfa4e453f64c44ee6ccbcd7274cbf30a89c7
Brent <brentw.white@gmail.com>
2016-04-28 23:11:59 -1000

    enhancements to project1 js and layout_1

--- src/js/project_pages/project1.js
+++ src/js/project_pages/project1.js
@@ -1,8 +1,7 @@
 $(window).load(function(){
     // Containers
     var imgContainers = $('.image-container');
-    var imgs = $('.img1,.img2,.img3,.img4');
-
+    var imgs = $('.img1,.img2,.img3,.img4,.img5,.img6');
     // Content
     var img1_discript = $('<div class="box_1"><p class="discript1">BOX dolor sit amet.</p></div>');
     var img2_discript = $('<div class="box_2"><p class="discript2">BOX2 dolor sit amet.</p></div>');
@@ -10,7 +9,8 @@ $(window).load(function(){
     var img4_discript = $('<div class="box_4"><p class="discript4">BOX4 dolor sit amet.</p></div>');
     var img5_discript = $('<div class="box_5"><p class="discript5">BOX5 dolor sit amet.</p></div>');
     var img6_discript = $('<div class="box_6"><p class="discript6">BOX6 dolor sit amet.</p></div>');
-
+    var all_boxes = $(img1_discript).add(img2_discript).add(img3_discript).add(img4_discript).add(img5_discript).add(img6_discript);
+    // indv img classes
     var img1 = $('.img1');
     var img2 = $('.img2');
     var img3 = $('.img3');
@@ -18,29 +18,18 @@ $(window).load(function(){
     var img5 = $('.img5');
     var img6 = $('.img6');
 
-    function removeBoxDiv (){
-        var mobile = Modernizr.mq('(max-width:972px)');
-
-        if (imgContainers.hasClass(imgs)); {
+    function removeBoxDiv (containsImgClass) {
+        containsImgClass = $(imgContainers.hasClass(imgs));
+        if (containsImgClass) {
             img1.append(img1_discript);
             img2.append(img2_discript);
             img3.append(img3_discript);
             img4.append(img4_discript);
             img5.append(img5_discript);
             img6.append(img6_discript);
-
-        } if (mobile) {
-            img1.find('div:last').remove();
-            img2.find('div:last').remove();
-            img3.find('div:last').remove();
-            img4.find('div:last').remove();
-            img5.find('div:last').remove();
-            img6.find('div:last').remove();
         }
     }
-
-    $(window).resize($.throttle(350,removeBoxDiv));
-
+    removeBoxDiv();
     // captionDisplay changes the css properties of the selected caption classes on device mq
     function captionDisplay (){
          // selecting caption classes
@@ -68,13 +57,12 @@ $(window).load(function(){
         // if smaller
             if (mobile_device) {
                 // visibility hidden
-                $(img1_discript).add(img2_discript).add(img3_discript).add(img4_discript).css('visibility','hidden');
-                images.css('height','515px');
+                all_boxes.css('visibility','hidden');
             }
             // if bigger
             else{
                 // visibility visible
-                $(img1_discript).add(img2_discript).add(img3_discript).add(img4_discript).css('visibility','visible');
+                all_boxes.css('visibility','visible');
             }
         }
     // throttle back resize event
